09865053c73ec78756da7ce4b72b47dfa98fd88f
daviddeftu-cloud <david.deftu@icloud.com>
2026-06-06 12:45:25 +0300

    Change activity name from 'over' to 'Watching over'

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "over Hexa Scripts.",
+        name: "Watching over Hexa Scripts.",
         // Activity type number (0 = Playing).
         type: 3, 
       },
